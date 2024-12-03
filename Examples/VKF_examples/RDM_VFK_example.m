d8b4b42cb076e3e682dd65626aff1007342df5bf
Antonino Visalli <50337337+antovis86@users.noreply.github.com>
2024-12-03 10:53:57 +0100

    Add files via upload

--- Examples/VKF_examples/RDM_VFK_example.m
+++ Examples/VKF_examples/RDM_VFK_example.m
@@ -10,7 +10,7 @@ lambda = .5; % VKF: lambda
 v0 = .5; % VKF: v0
 omega = .1; % VKF: omega
 a_a = 2; % RDM: Intercept of decision threshold "a"
-b_a = 1.2; % RDM: Muhat slope effect for "a"
+b_a = -1.2; % RDM: Muhat slope effect for "a"
 a_v = 2.5; % RDM: Intercept of drift rate "v"
 b_val = 2.5; % RDM: Effect of validity (resp = input) on the drift
 b_v = 0; % RDM: Muhat slope effect for "v"
@@ -23,8 +23,8 @@ muhat = 1./(1+exp(-muhat));
 
 % SIMULATE RESPONSES
 % Calculate trial-wise threshold for both the accumulators
-a_c1 = a_a + b_a.*(.5-muhat);
-a_c0 = a_a + b_a.*(.5-(1-muhat));
+a_c1 = a_a + b_a.*(muhat - .5);
+a_c0 = a_a + b_a.*((1-muhat) - .5);
 
 % Calculate drift rate for both accumulators
 drift_c1 = a_v + b_val.*(u==1) +  b_v .* (muhat - .5);
